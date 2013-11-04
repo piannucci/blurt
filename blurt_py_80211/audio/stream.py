6caa51f5019b5f56cf6f20d17b05354d56999056
Peter Iannucci <iannucci@mit.edu>
2013-11-03 20:28:45 -0500

    Compatibility fixes
    Conflicts:
    	blurt_cpp_80211/Makefile.osx

--- blurt_py_80211/audio/stream.py
+++ blurt_py_80211/audio/stream.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import numpy as np
 import coreaudio as ca
 import thread
@@ -148,10 +149,36 @@ class Visualizer(ThreadedStream):
         super(Visualizer, self).init()
         import pylab as pl
         self.pl = pl
-        self.fig = pl.figure()
+        dpi = pl.rcParams['figure.dpi']
+        import AppKit
+        screens = AppKit.NSScreen.screens()
+        mainScreen = AppKit.NSScreen.mainScreen()
+        secondaryScreens = set(screens).difference(set([mainScreen]))
+        if len(secondaryScreens):
+            screen = iter(secondaryScreens).next()
+        else:
+            screen = mainScreen
+        origin = screen.frame().origin
+        screensize = screen.frame().size
+        scale = screen.backingScaleFactor()
+        self.fig = pl.figure(figsize=(screensize.width/dpi, screensize.height/dpi), facecolor='k', edgecolor='k')
+        mgr = self.fig.canvas.manager
+        class ignore:
+            def __init__(self):
+                pass
+            def __call__(self, *args, **kwargs):
+                pass
+            def __getattr__(self, attr):
+                return self
+        mgr.toolbar = ignore()
+        mgr.window.wm_geometry("=%dx%d%+d%+d" % (screensize.width, screensize.height, .75*origin.x, origin.y))
+        mgr.resize(screensize.width, screensize.height)
+        mgr.set_window_title('Blurt')
+        #self.fig.set_figwidth(screensize.width/dpi)
+        #self.fig.set_figheight(screensize.height/dpi)
         self.ax = pl.gca()
-        widget = self.fig.canvas.get_tk_widget()
-        widget.winfo_toplevel().lift()
+        #widget = self.fig.canvas.get_tk_widget()
+        #widget.winfo_toplevel().lift()
         ca.sleepDuration = .01
     def draw(self):
         self.onMainThread(self.pl.draw)
@@ -165,47 +192,130 @@ class Oscilloscope(Visualizer):
         self.line.set_ydata(sequence)
         self.draw()
 
+def set_foregroundcolor(ax, color):
+     '''For the specified axes, sets the color of the frame, major ticks,
+         tick labels, axis labels, title and legend
+     '''
+     for tl in ax.get_xticklines() + ax.get_yticklines():
+         tl.set_color(color)
+     for spine in ax.spines:
+         ax.spines[spine].set_edgecolor(color)
+     for tick in ax.xaxis.get_major_ticks():
+         tick.label1.set_color(color)
+     for tick in ax.yaxis.get_major_ticks():
+         tick.label1.set_color(color)
+     ax.axes.xaxis.label.set_color(color)
+     ax.axes.yaxis.label.set_color(color)
+     ax.axes.xaxis.get_offset_text().set_color(color)
+     ax.axes.yaxis.get_offset_text().set_color(color)
+     ax.axes.title.set_color(color)
+     lh = ax.get_legend()
+     if lh != None:
+         lh.get_title().set_color(color)
+         lh.legendPatch.set_edgecolor('none')
+         labels = lh.get_texts()
+         for lab in labels:
+             lab.set_color(color)
+     for tl in ax.get_xticklabels():
+         tl.set_color(color)
+     for tl in ax.get_yticklabels():
+         tl.set_color(color)
+
+def set_backgroundcolor(ax, color):
+     '''Sets the background color of the current axes (and legend).
+         Use 'None' (with quotes) for transparent. To get transparent
+         background on saved figures, use:
+         pp.savefig("fig1.svg", transparent=True)
+     '''
+     ax.patch.set_facecolor(color)
+     lh = ax.get_legend()
+     if lh != None:
+         lh.legendPatch.set_facecolor(color)
+
 class SpectrumAnalyzer(Visualizer):
     def init(self):
         super(SpectrumAnalyzer, self).init()
         self.Fs = self.kwarg('Fs', 96000.)
         self.Fc = self.kwarg('Fc', 0.)
-        self.NFFT = self.kwarg('NFFT', 256)
-        self.noverlap = self.kwarg('noverlap', 128)
+        self.NFFT = self.kwarg('NFFT', 2048)
+        self.noverlap = self.kwarg('noverlap', 2048-256)
         self.window = self.kwarg('window', self.pl.mlab.window_hanning)(np.ones(self.NFFT))
         self.duration = self.kwarg('duration', 1.)
         self.transpose = self.kwarg('transpose', False)
         self.sides = self.kwarg('sides', 1)
         self.advance = self.NFFT - self.noverlap
         self.columns = int(round(self.duration * self.Fs / self.advance))
-        self.buffer = np.zeros((self.NFFT*self.sides/2, self.columns), np.float32)
+        self.buffer = np.zeros((self.NFFT*self.sides/2, self.columns*2), np.float32)
         self.t = 0.
+        self.window_phase = 0
+        box = self.ax.bbox.bounds
+        #self.fim = self.fig.figimage(self.buffer if not self.transpose else self.buffer.T,
+        #                             xo=box[0], yo=box[1])
+        self.fim = self.ax.imshow(self.buffer[:,:self.columns] if not self.transpose else self.buffer[:,:self.columns].T,
+                                  vmin=0., vmax=1., extent=(self.t, self.t+self.duration, 0, self.Fs/2000.), aspect='auto', interpolation='none')
+        self.ax.set_xlim(self.t, self.t+self.duration)
+        self.ax.set_ylim(0, self.Fs/2000.)
+        self.ax.patch.set_visible(False)
+        self.ax.set_xlabel('Time (seconds)')
+        self.ax.set_ylabel('Frequency (kHz)')
+        self.ax.set_title('Time-Frequency Visualizer')
+        self.fig.patch.set_facecolor('k')
+        set_backgroundcolor(self.ax, 'k')
+        set_foregroundcolor(self.ax, 'w')
         #self.im = self.ax.imshow(self.buffer if not self.transpose else self.buffer.T,
         #                         vmin=0., vmax=1.,
         #                         extent=(self.t, self.t+self.duration, -self.Fs/2, self.Fs/2),
         #                         aspect='auto', interpolation='none')
-        self.im = self.fig.figimage(self.buffer if not self.transpose else self.buffer.T,
-                                    xo=10, yo=10)
-        self.fig.delaxes(self.ax)
-        self.input_fragment = np.zeros(0, np.complex128)
+        #self.fig.delaxes(self.ax)
+        self.input_fragment = np.zeros(0, np.complex64)
     def thread_consume(self, input):
         stream = np.r_[self.input_fragment, input]
         n = max(1 + (stream.size-self.NFFT) // self.advance, 0)
         if n < 4: n = 0
         self.input_fragment = stream[n*self.advance:]
         if n > 0:
-            input = stream[:self.NFFT + (n-1)*self.advance]
             idx = np.arange(self.NFFT)[:,np.newaxis] + self.advance * np.arange(n)[np.newaxis,:]
-            input = input[idx] * self.window[:,np.newaxis]
+            input = stream[idx] * self.window[:,np.newaxis]
             input = np.fft.fftshift(np.fft.fft(input, axis=0), 0)
             input = (np.log10(np.abs(input)) / 5.) + 1
-            self.buffer[:,:-n] = self.buffer[:,n:]
-            self.buffer[:,-n:] = input[:(2-self.sides)*self.NFFT/2]
-            self.im.set_data(self.buffer if not self.transpose else self.buffer.T)
-            #self.im.set_extent((self.t, self.t+self.duration, -self.Fs/2, self.Fs/2))
-            #self.ax.set_xlim(self.t, self.t+self.duration)
-            #self.ax.set_ylim(0, self.Fs/2)
-            self.t += n * self.advance / self.Fs
-            self.fig.draw_artist(self.im)
-            self.onMainThread(self.fig.canvas.blit)
+            k = np.array([-.1,-.3,1.2,-.3,-.1]) * 1.5
+            for i in xrange(input.shape[1]):
+                input[:,i] = np.convolve(input[:,i], k, 'same')
+            input = input[:(2-self.sides)*self.NFFT/2]
 
+            cols = self.columns
+            phase = self.window_phase
+
+            if phase+cols+n <= 2*cols:
+                self.buffer[:,phase+cols:phase+cols+n] = input
+            else:
+                m = phase+n-cols
+                self.buffer[:,m-n:] = input[:,:n-m]
+                self.buffer[:,0:m] = input[:,-m:]
+            self.buffer[:,phase:phase+n] = input
+
+            phase = (phase + n) % cols
+            self.window_phase = phase
+
+            t = self.t
+            b = self.buffer[:,phase:phase+cols]
+            self.fim.set_data(b if not self.transpose else b.T)
+            self.fim.set_extent((t, t+self.duration, 0, self.Fs/2000.))
+            self.ax.set_xlim(t, t+self.duration)
+            def onMainThread():
+                self.fig.draw_artist(self.fig)
+                self.fig.canvas.blit()
+            self.onMainThread(onMainThread)
+            self.t += float(n * self.advance) / self.Fs
+
+def go():
+    import matplotlib
+    matplotlib.use('TkAgg')
+    matplotlib.rcParams['toolbar'] = 'None'
+    import coreaudio
+    Fs = 48000
+    sa = SpectrumAnalyzer(duration=3, Fs=Fs)
+    coreaudio.record(sa, Fs)
+
+if __name__ == '__main__':
+    pass #go()
