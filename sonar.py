e2d78a00585d5c36ca9f7f309fb31afe2e01942f
Peter Iannucci <iannucci@mit.edu>
2015-10-16 19:34:12 -0400

    Updated sonar demos

--- sonar.py
+++ sonar.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
-import audio, numpy as np, pylab as pl, iir, scipy.signal
-outputUID = 'AppleHDAEngineOutput:1B,0,1,1:0'
-externalMicUID = u'AppleUSBAudioEngine:Creative Technology:SB Live! 24-bit External:14100000:2,1'
-internalMicUID = u'AppleHDAEngineInput:1B,0,1,0:1'
-output = [i for i,d in enumerate(audio.devices()) if d['deviceUID'] == outputUID][0]
-externalMic = next((i for i,d in enumerate(audio.devices()) if d['deviceUID'] == externalMicUID), None)
-internalMic = next((i for i,d in enumerate(audio.devices()) if d['deviceUID'] == internalMicUID), None)
-Fs=96000
+import audio
+import numpy as np
+import pylab as pl
+import matplotlib.animation as animation
+import iir
+import scipy.signal
+import time
 
 def chirp(f0, f1, length):
     return np.exp(1j*2*np.pi*(np.arange(length)*float(f1-f0)/length + f0).cumsum())
@@ -14,7 +13,7 @@ def chirp(f0, f1, length):
 class SonarTransciever(audio.stream.Visualizer):
     def __init__(self):
         self.Fs = Fs = 96e3
-        self.pulse_low = 20e3
+        self.pulse_low = 10e3
         self.pulse_high = 30e3
         self.pulse_duration = int(.1 * Fs)
         self.display_duration = 3.
@@ -26,14 +25,18 @@ class SonarTransciever(audio.stream.Visualizer):
         pulse *= np.clip(-np.arange(-self.pulse_duration, 0, dtype=float)/(Fs*.025), 0, 1)
         mono_timing = np.tile(np.r_[1, np.zeros(spacing*2-1)], 3*simultaneous_pulses)
         mono = scipy.signal.fftconvolve(mono_timing, pulse, 'full')[(simultaneous_pulses-1)*spacing*2:simultaneous_pulses*spacing*2]
-        self.output = np.vstack((mono, np.roll(mono, spacing))).T
+        self.output = np.tile(np.vstack((mono, np.roll(mono, spacing))).T, (20,1))
         self.lpf = iir.lowpass((self.pulse_high-self.pulse_low)/Fs/2)
         cs = 1116.44 # feet/sec
         self.columns = int(self.display_duration*Fs) // (spacing*2)
-        self.buffer = np.zeros((2, spacing, self.columns*2), np.float32)
+        self.buffer = np.zeros((2, spacing, self.columns*2), np.uint16)
         self.window_phase = 0
         self.input_fragment = np.zeros(0, np.complex64)
         self.extent = (0, self.display_duration, -spacing*.1/Fs*cs/2, spacing*.9/Fs*cs/2)
+        self.i = 0
+        self.phase_locked = None
+        super().__init__(channels=2, outBufSize=16384, inBufSize=8192)
+        self.fig.delaxes(self.ax)
         self.ax1 = self.fig.add_subplot(1,2,1)
         self.im1 = self.ax1.imshow(self.buffer[0], vmin=0., vmax=10., extent=self.extent, aspect='auto', interpolation='nearest')
         self.ax1.set_xlim(self.extent[:2])
@@ -51,9 +54,7 @@ class SonarTransciever(audio.stream.Visualizer):
         audio.stream.set_foregroundcolor(self.ax1, 'w')
         audio.stream.set_backgroundcolor(self.ax2, 'k')
         audio.stream.set_foregroundcolor(self.ax2, 'w')
-        self.i = 0
-        self.phase_locked = None
-        super().__init__(channels=2)
+        self.cm = pl.get_cmap('jet')(np.arange(0,65536)/65535., bytes=True)
     def thread_produce(self):
         return self.output
     def thread_consume(self, input):
@@ -63,16 +64,14 @@ class SonarTransciever(audio.stream.Visualizer):
         lookahead += advance # needed for flash suppression
         if self.phase_locked is None:
            lookahead += self.spacing-1 # needed for phase lock
-        n = (stream.size-lookahead) // advance
-        if n < 40: n = 0
+        n = max(0, (stream.size-lookahead) // advance)
         actual_advance = n*advance
         if n > 0:
             try:
                 y = stream[:(n+1)*advance+lookahead]
                 Omega_carrier = 2*np.pi*(self.pulse_low+self.pulse_high)*.5/self.Fs
                 carrier = np.exp(1j*Omega_carrier*np.arange(self.i, self.i+y.size))
-                y = (carrier.conj() * y).astype(np.complex128)
-                y = self.lpf(y) * carrier # ehhh... we're time-warping the filter input
+                y = self.lpf(carrier.conj() * y) * carrier
                 y = scipy.signal.fftconvolve(y,self.base_chirp.conj()[::-1],'valid')
                 if self.phase_locked is None:
                     self.phase_locked = (np.abs(y).argmax()-int(self.spacing*.1)) % self.spacing
@@ -80,6 +79,8 @@ class SonarTransciever(audio.stream.Visualizer):
                     actual_advance += self.phase_locked
                 y = y[:(n+1)*advance].reshape(n+1,2,self.spacing).transpose(1,0,2)
                 y = np.log10(np.abs(np.diff(y, axis=1))).transpose(0,2,1)[:,::-1]
+                y *= .1 * 65535
+                y = np.clip(y, 0, 65536)
                 cols = self.columns
                 phase = self.window_phase
                 if phase+cols+n <= 2*cols:
@@ -88,123 +89,38 @@ class SonarTransciever(audio.stream.Visualizer):
                     m = phase+n-cols
                     self.buffer[:,:,m-n:] = y[:,:,:n-m]
                     self.buffer[:,:,0:m] = y[:,:,-m:]
+                self.window_phase = (phase + n) % cols
                 self.buffer[:,:,phase:phase+n] = y
-                phase = (phase + n) % cols
-                self.window_phase = phase
-                b = self.buffer[:,:,phase:phase+cols]
-                t = self.i / self.Fs
-                self.extent = (t, t+self.display_duration) + self.extent[2:]
-                self.im1.set_extent(self.extent)
-                self.im2.set_extent(self.extent)
-                self.ax1.set_xlim(t, t+self.display_duration)
-                self.ax2.set_xlim(t, t+self.display_duration)
-                self.im1.set_data(b[0])
-                self.im2.set_data(b[1])
-                self.draw()
             except (Exception,) as e:
                 import traceback
                 traceback.print_exc()
         self.input_fragment = stream[actual_advance:]
         self.i += actual_advance
+    def updatefig(self, *args):
+        t = self.i / self.Fs
+        self.extent = (t, t+self.display_duration) + self.extent[2:]
+        self.im1.set_extent(self.extent)
+        self.im2.set_extent(self.extent)
+        self.ax1.set_xlim(t, t+self.display_duration)
+        self.ax2.set_xlim(t, t+self.display_duration)
+        ticks = np.arange(t, t+self.display_duration+1e-4, .5)
+        ticklabels = ['%.1f' % x for x in ticks]
+        self.ax1.set_xticks(ticks)
+        self.ax2.set_xticks(ticks)
+        self.ax1.set_xticklabels(ticklabels)
+        self.ax2.set_xticklabels(ticklabels)
+        phase = self.window_phase
+        b = self.buffer[:,:,phase:phase+self.columns]
+        self.im1.set_data(self.cm[b[0]])
+        self.im2.set_data(self.cm[b[1]])
 
-def sonar(recv_only=False):
-    Fs = 96e3
-    pulse_low = 20e3
-    pulse_high = 30e3
-    pulse_duration = int(.1 * Fs)
-    spacing = int(.02 * Fs)
-    reps = 30
-    simultaneous_pulses = (pulse_duration+2*spacing-1) // (2*spacing)
-    base_chirp = chirp(pulse_low/Fs, pulse_high/Fs, pulse_duration)
-    lpf = iir.lowpass((pulse_high-pulse_low)/Fs/2)
-    cs = 1116.44 # feet/sec
-    extent = (0, reps*spacing*2/Fs, -spacing*.1/Fs*cs/2, spacing*.9/Fs*cs/2)
-    if recv_only:
-        y = audio.record(spacing*2*reps, Fs)
-    else:
-        pulse = base_chirp.real
-        pulse *= np.clip(np.arange(pulse_duration, dtype=float)/(Fs*.025), 0, 1)
-        pulse *= np.clip(-np.arange(-pulse_duration, 0, dtype=float)/(Fs*.025), 0, 1)
-        mono_timing = np.tile(np.r_[1, np.zeros(spacing*2-1)], 3*simultaneous_pulses)
-        mono = scipy.signal.fftconvolve(mono_timing, pulse, 'full')[(simultaneous_pulses-1)*spacing*2:simultaneous_pulses*spacing*2]
-        output = np.vstack((mono, np.roll(mono, spacing))).T
-        y = audio.play_and_record(np.tile(output, (reps, 1)), Fs)[1]
-    y = y.astype(np.complex128)
-    carrier = np.exp(1j*2*np.pi*(pulse_low+pulse_high)*.5/Fs*np.arange(y.size))
-    y = lpf(carrier.conj()*y) * carrier
-    y = scipy.signal.fftconvolve(y,base_chirp.conj()[::-1],'full')
-    y = y[(np.abs(y).argmax()-spacing*.1) % spacing:]
-    y = y[:y.size//spacing*spacing].reshape(-1,spacing)
-    y = y[:y.shape[0]//2*2]
-    y = y.reshape(y.shape[0]/2,2,-1).transpose(1,0,2)
-    y = np.log10(np.abs(np.diff(y, axis=1))).transpose(0,2,1)[:,::-1]
-    pl.clf()
-    pl.subplot(121)
-    pl.imshow(y[0], extent=extent, aspect='auto', interpolation='nearest')
-    pl.grid()
-    pl.subplot(122)
-    pl.imshow(y[1], extent=extent, aspect='auto', interpolation='nearest')
-    pl.grid()
-
-audio.play_and_record(SonarTransciever(), Fs)
-
-while True:
-    sonar(recv_only=True)
-    pl.draw()
-
-def align(x):
-    offset = np.abs(np.fft.ifft(np.fft.fft(x[:,1:], axis=0) * np.fft.fft(x[:,0:1], axis=0).conj(), axis=0)).argmax(0)
-    y = np.zeros_like(x)
-    y[:,0] = x[:,0]
-    for i in xrange(1,x.shape[1]):
-        y[:-offset[i-1],i] = x[offset[i-1]:,i]
-    return y
-
-def spline(weights):
-    order = len(weights)-1
-    import sympy, sympy.abc
-    x,y = sympy.abc.x, sympy.abc.y
-    polys = [np.poly1d(np.array(p.as_poly(x).all_coeffs(), dtype=float)) for p in (((x + y)**order).expand().subs(y, 1-x)).as_ordered_terms()][::-1]
-    return sum(p*c for p,c in zip(polys, weights))
-
-def sweep(start=0, end=48e3, duration=10, volume=1., rampLength=10e-3, outDevice=None, inDevices=None):
-    t = np.arange(Fs*duration, dtype=float)/Fs
-    f = (end-start)*t/duration + start
-    if callable(volume):
-        volume = volume(f)
-    ramp = np.clip(t/rampLength, 0, 1) * np.clip((duration-t)/rampLength, 0, 1)
-    waveform = volume * np.exp(1j*2*np.pi*f.cumsum()/Fs).real * ramp
-    results = audio.play_and_record(waveform, Fs, outDevice, inDevices)
-    results = np.vstack(results[1:])[:,:waveform.size]
-    return np.vstack((waveform, results)).T
-
-def plotSweep(results):
-    pl.clf()
-    pl.subplot(311)
-    pl.specgram(results[:,0], Fs=Fs, NFFT=1024, noverlap=768)
-    pl.xlim(0,results.shape[0]/Fs)
-    pl.ylim(0,48e3)
-    pl.subplot(312)
-    pl.specgram(results[:,1], Fs=Fs, NFFT=1024, noverlap=768)
-    pl.xlim(0,results.shape[0]/Fs)
-    pl.ylim(0,48e3)
-    pl.subplot(313)
-    pl.specgram(results[:,2], Fs=Fs, NFFT=1024, noverlap=768)
-    pl.xlim(0,results.shape[0]/Fs)
-    pl.ylim(0,48e3)
-    if 0:
-        lpf = iir.lowpass(500./Fs)
-        _=pl.plot(f[:-5000:50], 20*np.log10(np.abs(lpf(lpf(align(x)[:-5000,1] * np.exp(1j*2*np.pi*f.cumsum()[:-5000]/Fs).conj())[::50] ))))
-        _=pl.plot(f[:-5000:50], np.unwrap(np.angle(lpf(lpf(align(x)[:-5000,1] * np.exp(1j*2*np.pi*f.cumsum()[:-5000]/Fs).conj())[::50] ))))
-
-results = sweep(outDevice=output, inDevices=(internalMic, externalMic), volume=(lambda s:(lambda f:s(f/Fs*2)))(spline([.1,.1,.5,.5,.5])))
-plotSweep(results)
-
-if 0:
-    samples = audio.record(Fs*2, Fs, externalMic)
-    samples = np.r_[samples, audio.record(Fs*2, Fs, internalMic)]
-    pl.clf()
-    _=pl.specgram(samples, Fs=Fs, NFFT=1024, noverlap=768)
-    pl.ylim(0,48e3)
-    pl.xlim(0,4)
-    pl.colorbar()
+xcvr = SonarTransciever()
+Fs = 96e3
+ap = audio.AudioInterface()
+try:
+    ap.play(xcvr, Fs)
+    ap.record(xcvr, Fs)
+    ani = animation.FuncAnimation(xcvr.fig, xcvr.updatefig, interval=10, blit=False)
+    pl.show()
+except KeyboardInterrupt:
+    ap.stop()
---
+++ sonar2.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+import numpy as np
+import pylab as pl
+import audio
+import sys
+import time
+
+# http://poincare.matf.bg.ac.rs/~ezivkovm/publications/primpol1.pdf
+polys = {
+    2: (1,),
+    3: (1,),
+    4: (1,),
+    5: (2,),
+    6: (1,),
+    7: (1,),
+    8: (1, 2, 7),
+    9: (4,),
+    10: (3,),
+    11: (2,),
+    12: (1, 2, 10),
+    13: (3, 5, 8),
+    14: (1, 11, 12),
+    15: (1,),
+    16: (10, 12, 15),
+    17: (3,),
+}
+
+def mls(n=13):
+    assert n in polys, "No primitive polynomial for n=%d" % n
+    poly = (0, n) + polys[n]
+    poly = np.array(poly)
+    r = 1
+    m = np.empty((1<<n)-1, np.uint8)
+    for i in range((1<<n)-1):
+        m[i] = r & 1
+        r = (r>>1) | (np.bitwise_xor.reduce(r >> poly) & 1)<<(n-1)
+    return m
+
+class Transciever:
+    def __init__(self, m):
+        self.period = m.size*2
+        m = m.astype(np.float32) * 2 - 1
+        m = np.concatenate((np.r_[m, m][:,None], np.r_[m, -m][:,None]), 1)
+        m *= 1e-3
+        self.inBufSize = 2048
+        self.outBufSize = 2048
+        targetLength = self.outBufSize+self.period-1
+        self.output = np.tile(m, ((targetLength+m.shape[0]-1) // m.shape[0], 1))
+        self.input_fragment = np.zeros(0, np.complex64)
+    def __len__(self):
+        return sys.maxsize
+    def __getitem__(self, sl):
+        start = sl.start % self.period
+        count = sl.stop - sl.start
+        return self.output[start:start+count]
+    def append(self, sequence):
+        stream = np.r_[self.input_fragment, sequence]
+        lookahead = self.pulse_duration-1 # needed for convolution
+        n = max(0, (stream.size-lookahead) // self.period)
+        advance = n*self.period
+        if n > 0:
+            y = stream[:(n+1)*advance+lookahead]
+        self.input_fragment = stream[advance:]
+        self.i += advance
+
+n = 11
+Fs = 96e3
+print('Period: %.1f ms' % (1000*2*((1<<n)-1)/Fs,))
+xcvr = Transciever(mls(n))
+ap = audio.AudioInterface()
+ap.play(xcvr, Fs)
+ap.record(xcvr, Fs)
+try:
+    while True:
+        time.sleep(1.)
+except KeyboardInterrupt:
+    ap.stop()
+
