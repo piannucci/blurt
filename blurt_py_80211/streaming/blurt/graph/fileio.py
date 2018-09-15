6a56fbb14988dd8879c8d91d397570937b0bef7f
Peter Iannucci <iannucci@mit.edu>
2018-09-15 16:05:23 -0400

    FileSource block with optional rate limiting

--- blurt_py_80211/streaming/blurt/graph/fileio.py
+++ blurt_py_80211/streaming/blurt/graph/fileio.py
@@ -1,5 +1,8 @@
-from .graph import Input, Output, Block
+import os
+import warnings
+import time
 import pickle
+from .graph import Input, Output, Block
 
 class FileSink(Block):
     inputs = [Input('shape')]
@@ -13,29 +16,53 @@ class FileSink(Block):
         for item, in self.input():
             pickle.dump(item, self.f, -1)
 
-# import numpy as np
-# from .threaded import SelectorBlock
-#
-# class FileSource(SelectorBlock):
-#     def __init__(self, fn, dtype, itemshape=(1,)):
-#         self.inputs = []
-#         self.outputs = [Output(dtype, itemshape)]
-#         itemsize = np.product(itemshape)
-#         self.f = np.memmap(fn, dtype, 'r')
-#         self.f = iter(self.f[:self.f.size//itemsize*itemsize].reshape((-1,) + itemshape))
-#         self.stopping = False
-#         self.running = False
-#         super().__init__()
-#
-#     def start(self):
-#         self.output_queues[0].maxsize = 64
-#         with self.cv
-#
-#     def process(self):
-#         while not self.output_queues[0].full():
-#             try:
-#                 item = next(self.f)
-#             except StopIteration:
-#                 self.stop()
-#                 break
-#             self.output_queues[0].put_nowait(item)
+class FileSource(Block):
+    # TODO don't load entire file into memory when rate is unlimited
+    def __init__(self, fn, dtype, shape, ratelimit=None, timeGranularity=.01):
+        self.inputs = []
+        self.outputs = [Output(dtype, shape)]
+        super().__init__()
+        if ratelimit is None:
+            st = os.stat(fn)
+            if st.st_size > 1e9:
+                warnings.warn('On graph startup, FileSource will load %d bytes of pickled data into memory' % st.st_size)
+        else:
+            self.maxQueue = int(ratelimit * timeGranularity * 1.5 + 1)
+        self.f = open(fn, 'rb')
+        self.ratelimit = ratelimit
+        self.timeGranularity = timeGranularity
+
+    def start(self):
+        super().start()
+        if self.ratelimit is not None:
+            self.tokens = 0
+            self.lastFired = self.startTime = time.monotonic()
+            self._timerFired()
+        else:
+            while True:
+                try:
+                    self.output_queues[0].put(pickle.load(self.f))
+                except EOFError:
+                    break
+
+    def stop(self):
+        if self.ratelimit is not None:
+            self.runloop.removeTimer(self.timer)
+        super().stop()
+
+    def _timerFired(self):
+        now = time.monotonic()
+        dt = now - self.lastFired
+        self.lastFired = now
+        self.tokens = min(
+            self.tokens + dt * self.ratelimit,
+            self.maxQueue - self.out_queues[0].qsize(),
+        )
+        while self.tokens >= 1:
+            try:
+                self.output_queues[0].put(pickle.load(self.f))
+            except EOFError:
+                self.output_queues[0].closed = True
+                return
+            self.tokens -= 1
+        self.timer = self.runloop.addTimer(self._timerFired, delay=self.timeGranularity)
