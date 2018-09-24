966740c5a7e77c2d044badf04ce3fe3ba4382158
Peter Iannucci <iannucci@mit.edu>
2018-09-24 15:44:16 -0400

    Merged loopback test into main script

--- blurt_py_80211/streaming/blurt/wifi80211.py
+++ blurt_py_80211/streaming/blurt/wifi80211.py
@@ -19,6 +19,7 @@ from .audio import AudioHardware as AH
 from .net.graph_adapter import TunnelSink, TunnelSource
 from .mac.graph_adapter import PacketDispatchBlock, FragmentationBlock, ReassemblyBlock
 from .phy.ieee80211a import Channel, IEEE80211aDecoderBlock, IEEE80211aEncoderBlock
+from .audio.graph_adapter import AudioBypass_Block
 
 ############################ Parameters ############################
 
@@ -27,6 +28,8 @@ _channel = Channel(48e3, 17.0e3, 8)
 audioFrameSize = 256
 vuThresh = 25
 recordToFile = False
+bypassAudio = False
+vuMeter = not bypassAudio
 
 ############################ Audio ############################
 
@@ -38,25 +41,32 @@ class BlurtTransceiver:
 
         if 1:
             # create blocks and stream processors
-            self.ios = IOSession()
-            self.ios.addDefaultInputDevice()
-            self.ios.addDefaultOutputDevice()
-            self.ios.negotiateFormat(AH.kAudioObjectPropertyScopeOutput,
-                minimumSampleRate=txchannel.Fs, maximumSampleRate=txchannel.Fs, outBufSize=audioFrameSize)
-            self.ios.negotiateFormat(AH.kAudioObjectPropertyScopeInput,
-                minimumSampleRate=rxchannel.Fs, maximumSampleRate=rxchannel.Fs, inBufSize=audioFrameSize)
-            self.ios_b = IOSession_Block(self.ios)
-            self.inputChannels = self.ios.nChannelsPerFrame(AH.kAudioObjectPropertyScopeInput)
-            self.outputChannels = self.ios.nChannelsPerFrame(AH.kAudioObjectPropertyScopeOutput)
+            sources = []
+            if not bypassAudio:
+                self.ios = IOSession()
+                self.ios.addDefaultInputDevice()
+                self.ios.addDefaultOutputDevice()
+                self.ios.negotiateFormat(AH.kAudioObjectPropertyScopeOutput,
+                    minimumSampleRate=txchannel.Fs, maximumSampleRate=txchannel.Fs, outBufSize=audioFrameSize)
+                self.ios.negotiateFormat(AH.kAudioObjectPropertyScopeInput,
+                    minimumSampleRate=rxchannel.Fs, maximumSampleRate=rxchannel.Fs, inBufSize=audioFrameSize)
+                self.ios_b = IOSession_Block(self.ios)
+                self.inputChannels = self.ios.nChannelsPerFrame(AH.kAudioObjectPropertyScopeInput)
+                self.outputChannels = self.ios.nChannelsPerFrame(AH.kAudioObjectPropertyScopeOutput)
+                self.agc = MicrophoneAGCAdapter()
+                self.csma = CSMAOutStreamAdapter(self.agc, vuThresh, self.outputChannels)
+                self.is_b = InStream_SourceBlock(self.ios)
+                self.os_b = OutStream_SinkBlock()
+                sources.append(self.is_b)
+                sources.append(self.ios_b)
+            else:
+                self.audio_loopback_b = AudioBypass_Block()
+                self.is_b = self.os_b = self.audio_loopback_b
             tunnels = list(utun_by_ll_addr.values())
             utun_other = dict(zip(tunnels, tunnels[::-1]))
-            self.agc = MicrophoneAGCAdapter()
-            self.csma = CSMAOutStreamAdapter(self.agc, vuThresh, self.outputChannels)
-            self.is_b = InStream_SourceBlock(self.ios)
             if recordToFile:
                 self.tee_b = Tee(2)
                 self.filesink_b = FileSink('in_stream.s16_%d' % self.inputChannels)
-            self.os_b = OutStream_SinkBlock()
             self.decoder_b = IEEE80211aDecoderBlock(rxchannel)
             self.encoder_b = IEEE80211aEncoderBlock(txchannel)
             self.encoder_b.preferredRate = 6
@@ -66,14 +76,16 @@ class BlurtTransceiver:
             self.fragmenters = [FragmentationBlock(utun.pdb) for utun in tunnels]
             self.tunnelSinks = [TunnelSink(utun) for utun in utun_by_ll_addr.values()]
             self.tunnelSources = [TunnelSource(utun, utun_other[utun].ll_addr) for utun in utun_by_ll_addr.values()]
+            sources.extend(self.tunnelSources)
 
         if 1:
             # assemble graph
-            sources = [self.is_b, self.ios_b] + self.tunnelSources
             if 1:
-                # ios -> agc -> is_b -> decoder_b -> dispatch_b -> reassembly_b -> sink_b
-                self.ios.inStream = self.agc
-                self.agc.stream = self.is_b
+                # is_b -> decoder_b -> dispatch_b -> reassembly_b -> sink_b
+                if not bypassAudio:
+                    # ios -> agc -> is_b
+                    self.ios.inStream = self.agc
+                    self.agc.stream = self.is_b
                 if recordToFile:
                     self.is_b.connect(0, self.tee_b, 0)
                     self.tee_b.connect(0, self.decoder_b, 0)
@@ -85,18 +97,20 @@ class BlurtTransceiver:
                 for reassembly_b, sink_b in zip(self.reassemblers, self.tunnelSinks):
                     reassembly_b.connect(0, sink_b, 0)
             if 1:
-                # source_b -> fragmentation_b -> arbiter_b -> encoder_b -> os_b -> csma -> ios
+                # source_b -> fragmentation_b -> arbiter_b -> encoder_b -> os_b
                 for i, (source_b, fragmentation_b) in enumerate(zip(self.tunnelSources, self.fragmenters)):
                     source_b.connect(0, fragmentation_b, 0)
                     fragmentation_b.connect(0, self.arbiter_b, i)
                 self.arbiter_b.connect(0, self.encoder_b, 0)
                 self.encoder_b.connect(0, self.os_b, 0)
-                self.csma.stream = self.os_b
-                self.ios.outStream = self.csma
-            self.g = Graph(sources, runloop=runloop)
+                if not bypassAudio:
+                    # os_b -> csma -> ios
+                    self.csma.stream = self.os_b
+                    self.ios.outStream = self.csma
+            self.graph = Graph(sources, runloop=runloop)
 
-        self.start = self.g.start
-        self.stop = self.g.stop
+        self.start = self.graph.start
+        self.stop = self.graph.stop
 
         # TODO
         # latency = ios.inLatency+ios.outLatency
@@ -124,11 +138,16 @@ if __name__ == '__main__':
     try:
         while True:
             time.sleep(.05)
-            vu = int(max(0, 80 + 10*np.log10(xcvr.agc.vu)))
-            bar = [' '] * 150
-            bar[:vu] = ['.'] * vu
-            bar[vuThresh+80] = '|'
-            sys.stderr.status(''.join(bar) + ' %3d' % vu)
+            if bypassAudio:
+                # keep a steady stream of zeros flowing when there isn't a packet
+                xcvr.audio_loopback_b.input_queues[0].put(np.zeros((1000,2)))
+                xcvr.graph.notify()
+            if vuMeter:
+                vu = int(max(0, 80 + 10*np.log10(xcvr.agc.vu)))
+                bar = [' '] * 150
+                bar[:vu] = ['.'] * vu
+                bar[vuThresh+80] = '|'
+                sys.stderr.status(''.join(bar) + ' %3d' % vu)
     except KeyboardInterrupt:
         pass
     xcvr.stop()
